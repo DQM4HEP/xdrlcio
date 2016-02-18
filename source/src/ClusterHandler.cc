c3255b0dfb9d7d5e4526a085537488c3b7030856
Remi Ete <remi.ete@gmail.com>
2016-02-18 22:45:37 +0000

    ClusterHandler implementation finished

--- source/src/ClusterHandler.cc
+++ source/src/ClusterHandler.cc
@@ -27,8 +27,11 @@
 
 
 #include "xdrlcio/ClusterHandler.h"
+#include "xdrlcio/ParticleIDHandler.h"
 
 #include "IOIMPL/ClusterIOImpl.h"
+#include "IMPL/LCFlagImpl.h"
+#include "EVENT/LCIO.h"
 
 namespace xdrlcio
 {
@@ -50,6 +53,67 @@ xdrstream::Status ClusterHandler::read(xdrstream::IODevice *pDevice, EVENT::LCOb
 	XDR_STREAM( pDevice->read<int32_t>( &type ) )
 	pCluster->setType(type);
 
+	XDR_STREAM( pDevice->read<float>( &pCluster->_energy ) )
+	XDR_STREAM( pDevice->read<float>( &pCluster->_energyError ) )
+	XDR_STREAM( pDevice->readStaticArray( &pCluster->_position[0] , 3 ) )
+
+	float errpos[ NERRPOS ];
+	XDR_STREAM( pDevice->readStaticArray( &errpos[0] , NERRPOS ) )
+	pCluster->setPositionError( errpos );
+
+	XDR_STREAM( pDevice->read<float>( &pCluster->_theta ) )
+	XDR_STREAM( pDevice->read<float>( &pCluster->_phi ) )
+
+	float errdir[ NERRDIR ];
+	XDR_STREAM( pDevice->readStaticArray( &errdir[0] , NERRDIR ) )
+	pCluster->setDirectionError( errdir );
+
+	uint32_t nShapes;
+	XDR_STREAM( pDevice->read( &nShapes ) )
+	pCluster->_shape.resize( nShapes );
+	XDR_STREAM( pDevice->readStaticArray( &pCluster->_shape[0] , nShapes ) )
+
+	uint32_t nPid;
+	XDR_STREAM( pDevice->read( &nPid ) )
+
+	for(uint32_t i=0 ; i<nPid ; i++)
+	{
+		EVENT::ParticleID *pParticleID = NULL;
+		XDR_STREAM( ParticleIDHandler::read( pDevice , pParticleID , m_version ) )
+
+		pCluster->addParticleID( pParticleID );
+	}
+
+	uint32_t nClusters;
+	XDR_STREAM( pDevice->read( &nClusters ) )
+	pCluster->_clusters.resize( nClusters );
+
+	for( uint32_t i=0 ; i<nClusters ; i++ )
+		XDR_STREAM( pDevice->readPointerReference( (void **) &pCluster->_clusters[i] ) )
+
+	if( IMPL::LCFlagImpl( m_lcFlag).bitSet( EVENT::LCIO::CLBIT_HITS ) )
+	{
+		uint32_t nHits;
+		XDR_STREAM( pDevice->read( &nHits ) )
+		pCluster->_hits.resize( nHits );
+		pCluster->_weights.resize( nHits );
+
+		for(uint32_t i=0 ; i<nHits ; i++)
+		{
+			XDR_STREAM( pDevice->readPointerReference( (void **) &pCluster->_hits[i] ) )
+			XDR_STREAM( pDevice->read( &pCluster->_weights[i] ) )
+		}
+	}
+
+	uint32_t nEnergies;
+	XDR_STREAM( pDevice->read( &nEnergies ) )
+	pCluster->_subdetectorEnergies.resize( nEnergies );
+
+	for(uint32_t i=0 ; i<nEnergies ; i++)
+		XDR_STREAM( pDevice->read( &pCluster->_subdetectorEnergies[i] ) )
+
+	XDR_STREAM( pDevice->readPointerTag( (void *) pCluster ) )
+
 	return xdrstream::XDR_SUCCESS;
 }
 
@@ -57,10 +121,65 @@ xdrstream::Status ClusterHandler::read(xdrstream::IODevice *pDevice, EVENT::LCOb
 
 xdrstream::Status ClusterHandler::write(xdrstream::IODevice *pDevice, const EVENT::LCObject *const pLCObject)
 {
-	const XdrCluster *const pCluster = dynamic_cast<const XdrCluster *const>( pLCObject );
+	const EVENT::Cluster *const pCluster = dynamic_cast<const EVENT::Cluster *const>( pLCObject );
 
 	int32_t type = pCluster->getType();
 	XDR_STREAM( pDevice->write<int32_t>( &type ) )
+
+	float energy = pCluster->getEnergy();
+	XDR_STREAM( pDevice->write<float>( &energy ) )
+
+	float energyError = pCluster->getEnergyError();
+	XDR_STREAM( pDevice->write<float>( &energyError ) )
+
+	XDR_STREAM( pDevice->writeArray( pCluster->getPosition() , 3 ) )
+	XDR_STREAM( pDevice->writeArray( &pCluster->getPositionError()[0] , NERRPOS ) )
+
+	float theta = pCluster->getITheta();
+	XDR_STREAM( pDevice->write<float>( &theta ) )
+
+	float phi = pCluster->getIPhi();
+	XDR_STREAM( pDevice->write<float>( &phi ) )
+
+	XDR_STREAM( pDevice->writeArray( &pCluster->getDirectionError()[0] , NERRDIR ) )
+
+	const EVENT::FloatVec &shape = pCluster->getShape();
+	uint32_t nShapes = shape.size();
+	XDR_STREAM( pDevice->write( &nShapes ) )
+
+	XDR_STREAM( pDevice->writeArray( &shape[0] , nShapes ) )
+
+	uint32_t nPid = pCluster->getParticleIDs().size();
+	XDR_STREAM( pDevice->write( &nPid ) )
+
+	for(uint32_t i=0 ; i<nPid ; i++)
+		XDR_STREAM( ParticleIDHandler::write( pDevice , pCluster->getParticleIDs()[i] ) )
+
+	uint32_t nClusters = pCluster->getClusters().size();
+	XDR_STREAM( pDevice->write( &nClusters ) )
+
+	for( uint32_t i=0 ; i<nClusters ; i++ )
+		XDR_STREAM( pDevice->writePointerReference( pCluster->getClusters()[i] ) )
+
+	if( IMPL::LCFlagImpl( m_lcFlag).bitSet( EVENT::LCIO::CLBIT_HITS ) )
+	{
+		uint32_t nHits = pCluster->getCalorimeterHits().size();
+		XDR_STREAM( pDevice->write( &nHits ) )
+
+		for(uint32_t i=0 ; i<nHits ; i++)
+		{
+			XDR_STREAM( pDevice->writePointerReference( pCluster->getCalorimeterHits()[i] ) )
+			XDR_STREAM( pDevice->write( &pCluster->getHitContributions()[i] ) )
+		}
+	}
+
+	uint32_t nEnergies = pCluster->getSubdetectorEnergies().size();
+	XDR_STREAM( pDevice->write( &nEnergies ) )
+
+	for(uint32_t i=0 ; i<nEnergies ; i++)
+		XDR_STREAM( pDevice->write( &pCluster->getSubdetectorEnergies()[i] ) )
+
+	XDR_STREAM( pDevice->writePointerTag( pCluster ) )
 
 	return xdrstream::XDR_SUCCESS;
 }
